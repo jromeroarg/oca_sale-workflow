d6d68b7c411ed8421f7bc31dad213785e6d87146
Jose Romero <jromero_arg@yahoo.com.ar>
2020-09-01 18:50:55 -0300

    Update res_config_settings.py

--- sale_order_line_description_or_name/models/res_config_settings.py
+++ sale_order_line_description_or_name/models/res_config_settings.py
@@ -9,6 +9,8 @@ class ResConfigSettings(models.TransientModel):
 
     group_use_product_description_or_name_per_so_line = fields.Boolean(
         "Permite usar name o description_sale en línea de orden de venta",
+        implied_group="sale_order_line_description_or_name."
+        "group_use_product_description_or_name_per_so_line",
         help="Permite usar o el nombre del producto, o la descripción del "
         " producto. Además puede anteponer el código del mismo"
         )
