fe0e53dfa716f1392db6191af3b553188b3262ff
Jose Romero <jromero_arg@yahoo.com.ar>
2020-09-01 19:34:19 -0300

    Version original 20200901

---
+++ sale_order_line_description_or_name/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale order line description or name",
+    "version": "11.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Agile Business Group, "
+              "Odoo Community Association (OCA),"
+              "jromero_arg",
+    "website": "https://github.com/jromeroarg/oca_sale-workflow/",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "security/sale_security.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "installable": True,
+}
